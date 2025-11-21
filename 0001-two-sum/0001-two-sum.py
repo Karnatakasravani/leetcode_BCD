e92a5d69ed27f97e9616833186dd63504dcd6b4b
Sravani_karnataka <119839721+Karnatakasravani@users.noreply.github.com>
2025-11-21 15:54:58 +0530

    Time: 0 ms (100%), Space: 18.8 MB (51.41%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -4,7 +4,7 @@ class Solution:
         # for i in range(len(nums)):
         #     dic[nums[i]] = i
         for num in range(len(nums)):
-            dif = abs(target - nums[num])
+            dif =(target - nums[num])
             if dif in dic:
                 return [num,dic[dif]]
             dic[nums[num]] = num 
